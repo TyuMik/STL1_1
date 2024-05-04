5cb939a6f3b233175cc8d781264f33980b655d9b
Mikhail <miktyu88@gmail.com>
2024-05-04 16:56:10 +0300

    Initial commit

---
+++ STL1_1/STL1_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите строку: ";
+    std::string string;
+    std::getline(std::cin, string);
+    
+    std::multiset<char> s;
+
+    for (const auto& i : string) {
+        s.insert(i);
+    }
+    
+    std::map<char, int> m;
+
+    for (const auto& i : s) {
+        m[i] = s.count(i);
+    }
+
+    std::multimap<int, char> m1;
+
+    for (const auto& i : m) {
+        m1.insert({ i.second, i.first });
+    }
+
+    std::multimap<int, char>::reverse_iterator itr;
+
+    for (auto itr = m1.rbegin(); itr != m1.rend(); itr++) {
+        std::cout << itr->second << " - " << itr->first << std::endl;
+    }
+
+    return 0;
+}
